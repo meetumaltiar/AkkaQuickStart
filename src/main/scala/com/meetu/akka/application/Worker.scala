bfbbb3a5d6bceab616bcfe0ab9c754c08bd7a55e
Meetu Maltiar <meetu.maltiar@gmail.com>
2014-09-01 12:46:47 +0530

    changing the accumulator name

--- src/main/scala/com/meetu/akka/application/Worker.scala
+++ src/main/scala/com/meetu/akka/application/Worker.scala
@@ -6,13 +6,13 @@ class Worker extends Actor {
 
   def receive = {
     case Work(start, nrOfElements) =>
-      sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
+      sender ! Result(calculatePiFor(start, nrOfElements))
   }
 
   def calculatePiFor(start: Int, nrOfElements: Int): Double = {
     var acc = 0.0
-    for (i <- start until (start + nrOfElements))
-      acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+    for (n <- start until (start + nrOfElements))
+      acc += 4.0 * (1 - (n % 2) * 2) / (2 * n + 1)
     acc
   }
 }
