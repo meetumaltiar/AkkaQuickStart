9395c80fce97746b2ff0049f051f410954e54229
Meetu Maltiar <meetu.maltiar@gmail.com>
2014-09-01 13:09:08 +0530

    Added a functional way of handling

--- src/main/scala/com/meetu/akka/application/Worker.scala
+++ src/main/scala/com/meetu/akka/application/Worker.scala
@@ -6,8 +6,14 @@ class Worker extends Actor {
 
   def receive = {
     case Work(start, nrOfElements) =>
-      sender ! Result(calculatePiFor(start, nrOfElements))
+      sender ! Result(calculatePiForElement(start, nrOfElements))
   }
+
+  def calculatePiForElement(start: Int, nrOfElements: Int): Double = {
+    (start until (start + nrOfElements)).foldLeft(0.0) { (acc, elem) => acc + calculateElement(elem) }
+  }
+
+  def calculateElement(n: Double): Double = 4.0 * (1 - (n % 2) * 2) / (2 * n + 1)
 
   def calculatePiFor(start: Int, nrOfElements: Int): Double = {
     var acc = 0.0
